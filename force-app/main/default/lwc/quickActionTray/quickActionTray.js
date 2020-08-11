f7028cd5a3ef3f2af4eef510d9a1f2c7b64391ec
Frank Caron <frank@frankcaron.com>
2020-08-11 12:16:57 -0400

    Adding cards

---
+++ force-app/main/default/lwc/quickActionTray/quickActionTray.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class QuickActionTray extends LightningElement {}
---
+++ force-app/main/default/lwc/taskTray/taskTray.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class TaskTray extends LightningElement {}
